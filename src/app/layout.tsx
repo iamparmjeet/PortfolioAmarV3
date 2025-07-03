f9b73bdb3414185a50a0a1470799f0c914f2807b
parm <iamparmjeetmishra@gmail.com>
2025-07-03 21:24:56 +0530

    container removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
         {/* <GridBackground /> */}
         <DotBackground />
         <NavHeader />
-        <main className="min-h-screen flex-grow  md:mx-auto z-30 relative container">
+        <main className="min-h-screen flex-grow z-30 relative ">
           {children}
           <Footer />
         </main>
