5a3ba6bb05b5fbe2717097bab65124fd5f479310
parm <iamparmjeetmishra@gmail.com>
2025-07-31 23:07:48 +0530

    RandomId generator

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function randomId(length = 8) {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
