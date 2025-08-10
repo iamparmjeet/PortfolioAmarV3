a3abae8f740a70f9f687f755c0aae7703879562c
parm <iamparmjeetmishra@gmail.com>
2025-08-10 11:27:29 +0530

    Modify->Container component updated with id props

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,13 +1,14 @@
 import { cn } from "@/lib/utils";
 
 type ContainerProps = {
+  id?: string;
   children: React.ReactNode;
   className?: string;
 };
 
-export default function Container({ children, className }: ContainerProps) {
+export default function Container({ id, children, className }: ContainerProps) {
   return (
-    <section className={cn("container mx-auto flex gap-6 bg-black/60 rounded-2xl overflow-hidden my-4 md:my-6 text-white p-6 z-30 shadow-2xl backdrop-blur-sm", className)}>
+    <section id={id} className={cn("container mx-auto flex gap-6 bg-black/60 rounded-2xl overflow-hidden my-4 md:my-6 text-white p-6 z-30 shadow-2xl backdrop-blur-sm", className)}>
       {children}
     </section>
   );
