c05280863ba3cb4744e41fd1c4ba73db782945d8
parm <iamparmjeetmishra@gmail.com>
2025-08-10 12:15:01 +0530

    Modify->NextVideo component makes it responsive by default

--- src/components/video/next-video.tsx
+++ src/components/video/next-video.tsx
@@ -1,31 +1,43 @@
 import Video from "next-video/player";
 import Image from "next/image";
-// import Instaplay from "player.style/instaplay/react";
+
+import { cn } from "@/lib/utils";
 
 type VideoProps = {
   href: string;
-  thumbnail?: string;
+  thumbnail: string; // main thumbnail.webp
+  className?: string;
 };
 
-export default function NextVideo({ href, thumbnail }: VideoProps) {
+export default function NextVideo({
+  href,
+  thumbnail,
+  className,
+}: VideoProps) {
   return (
-    <Video
-      src={href}
-      className="w-full h-full object-cover"
-      autoPlay={false}
-      muted={false}
-      controls={true}
-      // theme={Instaplay}
+    <div
+      className={cn(
+        "aspect-[9/16] w-full max-w-xs md:max-w-xl overflow-hidden rounded-xl shadow-lg relative",
+        className,
+      )}
     >
-      <Image
-        slot="poster"
-        src={thumbnail || ""}
-        placeholder="blur"
-        blurDataURL={thumbnail}
-        width={480}
-        height={855}
-        alt="Video thumbnail"
-      />
-    </Video>
+      <Video
+        src={href}
+        className="w-full h-full object-cover"
+        autoPlay={false}
+        muted={false}
+        controls={true}
+      >
+        <Image
+          slot="poster"
+          src={thumbnail}
+          placeholder="blur"
+          blurDataURL={thumbnail.replace("thumbnail.webp", "blur-thumbnail.webp")}
+          width={480}
+          height={855}
+          alt="Video thumbnail"
+        />
+      </Video>
+    </div>
   );
 }
