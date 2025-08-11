5813b48d00a59359fb34d15fbce9f6d12ed57e83
parm <iamparmjeetmishra@gmail.com>
2025-08-11 17:57:10 +0530

    Modify->Socials updated for isrow without label option

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -2,6 +2,7 @@ import { IconBrandInstagram, IconBrandTwitter, IconBrandYoutube } from "@tabler/
 import Link from "next/link";
 
 import { socials } from "@/lib/data";
+import { cn } from "@/lib/utils";
 
 type SocialsType = {
   id: number;
@@ -43,15 +44,22 @@ const Socials: SocialsType[] = [
   },
 ];
 
-export default function SocialsBox() {
+type SocialBoxProps = {
+  isRow?: boolean;
+  className?: string;
+};
+
+export default function SocialsBox({ isRow = false, className }: SocialBoxProps) {
   return (
-    <div className="space-y-4 ">
+    <div className={cn(isRow ? "flex gap-4" : "space-y-4")}>
       {Socials.map(social => (
         <SocialIcon
           key={social.id}
           href={social.href}
           icon={social.icon}
           label={social.label}
+          isRow={isRow}
+          className={className}
         />
       ))}
     </div>
@@ -62,16 +70,22 @@ type SocialIconType = {
   href: string;
   label: string;
   icon: React.ReactNode;
+  isRow: boolean;
+  className?: string;
 };
 
-function SocialIcon({ href, icon, label }: SocialIconType) {
+function SocialIcon({ href, icon, label, isRow, className }: SocialIconType) {
   return (
     <Link
       href={href}
-      className="flex items-center gap-2 text-neutral-400 hover:text-neutral-300 transition-colors"
+      className={cn(
+        className,
+        "flex items-center gap-2 text-neutral-400 hover:text-neutral-300 transition-colors",
+        (isRow ? "flex-col" : "flex-row"),
+      )}
     >
       {icon}
-      <p>{ label }</p>
+      {!isRow && <p>{ label }</p>}
     </Link>
   );
 }
